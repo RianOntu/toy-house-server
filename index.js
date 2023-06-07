a0f74b0fd9d291dfacd2843f1ff830ec72a77d02
RianOntu <rianontu13@gmail.com>
2023-06-07 11:34:50 +0600

    created basic server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const port=process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -28,3 +35,8 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("server is running")
+})
+app.listen(port);
